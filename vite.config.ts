07bb95d47fac2a40b5d879c67e902307181fd627
MaitLattekivi <mait.lattekivi@pipedrive.com>
2025-05-20 14:14:30 +0300

    AIHX-6: merge package jsons

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: 'client',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'client/src'),
+    },
+  },
+})
